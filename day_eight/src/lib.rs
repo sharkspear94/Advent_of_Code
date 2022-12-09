89b84801c8efbcddb3c6d6776cee13f1fb6e1d96
FelixKuhn <felix.kuhnkuhn@gmail.com>
2022-12-09 03:47:22 +0100

    day 8 finished

---
+++ day_eight/src/lib.rs
@@ -0,0 +1,145 @@
+fn process_one(input: &str) -> String {
+    let width = input.chars().take_while(|c|!c.is_ascii_whitespace()).count();
+
+    let v = input.lines()
+        .flat_map(|line| line.chars()).filter(|c| c.is_ascii_digit())
+        .collect::<Vec<_>>();
+    let seen = v.iter()
+        .enumerate()
+        .map(|(i,c)| traverse(c, i, width, &v[..]))
+        .filter(|a| *a)
+        .count();
+    seen.to_string()
+}
+
+fn traverse(c: &char,idx: usize,len: usize, s: &[char]) -> bool {
+    let right_bound = (((idx/len)+1)*len) -1;
+    let left_bound = idx/len * len;
+    right(c,&s[idx..=right_bound]) || left(c, &s[left_bound..idx])
+     || up(c, len, &s[..=idx]) || down(c, len, &s[idx..]) 
+}
+
+fn right(c: &char, s: &[char]) -> bool {
+    if s.len() > 0 {
+        !s.iter().skip(1).any(|tree| tree >= c)
+    } else {true}
+}
+
+fn left(c: &char, s: &[char]) -> bool {
+    if s.len() > 0 {
+        !s.iter().rev().any(|tree| tree >= c)
+    } else {true}
+}
+
+fn up(c: &char,len: usize, s: &[char]) -> bool {
+    if s.len() >= len {
+        !s.iter().rev().step_by(len).skip(1).any(|tree| tree >= c)
+    } else {true}
+}
+
+fn down(c: &char,len: usize, s: &[char]) -> bool {
+    if s.len() >= len {
+        !s.iter().step_by(len).skip(1).any(|tree| tree >= c)
+    } else {true}
+}
+
+
+// ------------------------------------------------------------------
+
+fn process_two(input: &str) -> String {
+    let width = input.chars().take_while(|c|!c.is_ascii_whitespace()).count();
+    
+    let v = input.lines()
+        .flat_map(|line| line.chars()).filter(|c| c.is_ascii_digit())
+        .collect::<Vec<_>>();
+    let seen = v.iter()
+        .enumerate()
+        .map(|(i,c)| traverse2(c, i, width, &v[..]))
+        // .inspect(|a| println!("{a}"))
+        .max().unwrap();
+    seen.to_string()
+}
+
+fn traverse2(c: &char,idx: usize,len: usize, s: &[char]) -> usize {
+    let right_bound = (((idx/len)+1)*len) -1;
+    let left_bound = idx/len * len;
+    right2(c,&s[idx..=right_bound]) * left2(c, &s[left_bound..idx])
+     * up2(c, len, &s[..=idx]) * down2(c, len, &s[idx..]) 
+}
+
+fn right2(c: &char, s: &[char]) -> usize {
+    if s.len() > 0 {
+        let c = s.iter().skip(1).take_while(|tree| tree < &c).count();
+        if s.iter().skip(1).skip(c).next().is_none() {
+            c
+        } else {c+1}
+    } else {0}
+}
+
+fn left2(c: &char, s: &[char]) -> usize {
+    if s.len() > 0 {
+        let c = s.iter().rev().take_while(|tree| tree < &c).count();
+        if s.iter().rev().skip(c).next().is_none() {
+            c
+        } else {c+1}
+    } else {0}
+}
+
+fn up2(c: &char,len: usize, s: &[char]) -> usize {
+    if s.len() > len {
+        let c = s.iter().rev().step_by(len).skip(1).take_while(|tree| tree < &c).count();
+        if s.iter().rev().step_by(len).skip(1).skip(c).next().is_none() {
+            c
+        } else {c+1}
+    } else {0}
+}
+
+fn down2(c: &char,len: usize, s: &[char]) -> usize {
+    if s.len() > len {
+        let c = s.iter().step_by(len).skip(1).take_while(|tree| tree < &c).count();
+        if  s.iter().step_by(len).skip(1).skip(c).next().is_none() {
+            c
+        } else {c+1}
+    } else {0}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn part_one() {
+        let input = std::fs::read_to_string("./input.txt").unwrap();
+        println!("{}",process_one(&input));
+
+    }
+
+    #[test]
+    fn part_two() {
+        let input = std::fs::read_to_string("./input.txt").unwrap();
+        println!("{}",process_two(&input));
+
+    }
+
+
+    #[test]
+    fn test_process_part_one() {
+        let input = 
+"30373
+25512
+65332
+33549
+35390";
+        assert_eq!(process_one(input), "21");
+    }
+
+    #[test]
+    fn test_process_part_two() {
+        let input = 
+"30373
+25512
+65332
+33549
+35390";
+        assert_eq!(process_two(input), "8");
+    }
+}
